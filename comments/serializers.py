fc705237093e66c2b7c47923b6d6b2a2a956b5a9
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-12-02 06:05:51 +0000

    Add comment list and detail views

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from comments.models import Comment
+from .models import Comment
+
 
 class CommentSerializer(serializers.ModelSerializer):
     """
@@ -8,22 +9,21 @@ class CommentSerializer(serializers.ModelSerializer):
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source = 'owner.profile.id')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
- 
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
         model = Comment
-
         fields = [
-        'id', 'owner', 'is_owner', 'profile_id',
-         'profile_image', 'created_at', 'updated_at',
-           'content', 'post'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
         ]
-       
+
+
 class CommentDetailSerializer(CommentSerializer):
     """
     Serializer for the Comment model used in Detail view
--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import  APIView
 from rest_framework.response import Response
 from .models import Profile
-from .serializer import ProfileSerializer
+from .serializers import ProfileSerializer
 from django.http import Http404
 from drf_api.permissions import IsOwnerOrReadOnly
 
